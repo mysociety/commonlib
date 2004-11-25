c7c407b196f8206b992725258af72cfec45b99f1
chris <chris>
2004-11-25 00:10:50 +0000

    Read config values using the PHP binary, rather than trying to parse some kind of crazy cod-PHP.

--- mySociety/Config.pm
+++ mySociety/Config.pm
@@ -6,15 +6,17 @@
 # Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
 # Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: Config.pm,v 1.9 2004-11-24 23:11:39 francis Exp $
+# $Id: Config.pm,v 1.10 2004-11-25 00:10:50 chris Exp $
 #
 
 package mySociety::Config;
 
 use strict;
 use IO::File;
+use IPC::Open2;
 use Error qw(:try);
 use Data::Dumper;
+use POSIX;
 
 =head1 NAME
 
@@ -48,65 +50,100 @@ sub fixup ($) {
     }
 }
 
+# find_php
+# Try to locate the PHP binary in various sensible places.
+sub find_php () {
+    foreach my $dir (split(/:/, $ENV{PATH}),
+        qw(/usr/local/bin /usr/bin /software/bin /opt/bin /opt/php/bin)) {
+        foreach my $name (qw(php4 php php4-cgi php-cgi)) {
+            return "$dir/$name" if (-x "$dir/$name");
+        }
+    }
+    die "unable to locate PHP binary, needed to read config file";
+}
+
 =item read_config FILE [DEFAULTS]
 
-Read configuration from FILE, which may be either a name or a filehandle.
-Returns a reference to a hash of parameter to value. Config files are in a
-sort of cod-PHP; basically only define('...', '...') is handled, though we
-do process comments. If specified, values from DEFAULTS are merged. (Most users
-should just call set_file and get, though. See below.)
+Read configuration from FILE, which should be the name of a PHP config file.
+This is parsed by PHP, and any defines with names beginning "OPTION_" are
+extracted as config values. If specified, values from DEFAULTS are merged.
 
 =cut
+my $php_path;
 sub read_config ($;$) {
     my ($f, $defaults) = @_;
-    my $filename = "<unknown>";
+    # We need to find the PHP binary.
+    $php_path ||= find_php();
 
-    if (!ref($f)) {
-        my $F = new IO::File($f, O_RDONLY) or die "$f: $!";
-        $filename = $f;
-        $f = $F;
+    # Safest way to pass the value into PHP....
+
+    my ($rd, $wr);
+    $ENV{MYSOCIETY_CONFIG_FILE_PATH} = $f;
+    my $pid = open2($rd, $wr, $php_path) or die "$php_path: $f: $!";
+    delete($ENV{MYSOCIETY_CONFIG_FILE_PATH});
+
+    $wr->print(<<'EOF');
+<?php
+
+require(getenv("MYSOCIETY_CONFIG_FILE_PATH"));
+
+$a = get_defined_constants();
+
+print "start_of_options\n";
+foreach ($a as $k => $v) {
+    if (preg_match("/^OPTION_/", $k)) {
+        print substr($k, 7); /* strip off "OPTION_" */
+        print "\0";
+        print $v;
+        print "\0";
+    }
+}
+
+?>
+EOF
+
+    $wr->close();
+
+    # skip any header material
+    my $line;
+    while (defined($line = $rd->getline())) {
+        last if ($line eq "start_of_options\n");
     }
 
-    my $text = join('', $f->getlines());
-    $f->close();
+    if (!defined($line)) {
+        if ($rd->error()) {
+            die "$php_path: $f: $!";
+        } else {
+            die "$php_path: $f: no option output from subprocess";
+        }
+    }
 
-    # Only at start of line, so that we don't find comments in the middle of
-    # strings (consider "http://...").
-    $text =~ s#^\s*//.*$##gm;
-    $text =~ s#/\*.+?\*/##gs;
+    # read remainder
+    my $buf = '';
+    my $n;
+    do {
+        $n = $rd->read($buf, 1024, length($buf));
+        die "$php_path: $f: $!" if (!defined($n));
+    } while ($n > 0);
 
-    my $config = { };
+    $rd->close();
+
+    my @vals = split(/\0/, $buf);
+    die "$php_path: $f: bad option output from subprocess" if (scalar(@vals) % 2);
+
+    my %config = @vals;
+
     if ($defaults) {
-        $config->{$_} = $defaults->{$_} foreach (keys %$defaults);
+        $config{$_} = $defaults->{$_} foreach (keys %$defaults);
     }
 
-    while ($text =~ m#
-                    define
-                    \s*
-                    \(
-                    \s*
-                    
-                    ('.+?'|".+?")
-                    
-                    \s*
-                    ,
-                    \s*
-                    
-                    ('.*?'|".*?"|\d+)
+    # Wait for PHP to finish. But someone else may have installed a handler for
+    # SIGCHLD, so don't try too hard.
+    waitpid($pid, POSIX::WNOHANG);
 
-                    \s*
-                    \)
-                    #xg) {
-        my ($key, $val) = ($1, $2);
-        fixup($key);
-        fixup($val);
-        $key =~ s/^OPTION_//;
-        $config->{$key} = $val;
-    }
+    $config{"CONFIG_FILE_NAME"} = $f;
 
-    $config->{"CONFIG_FILE_NAME"} = $filename;
-
-    return $config;
+    return \%config;
 }
 
 =item set_file FILENAME
