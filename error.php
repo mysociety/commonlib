0b5abec8f371aeb3e69a0bf0cf93fe878316b7ca
chris <chris>
2005-01-10 15:37:20 +0000

    Doomed attempt to implement sane error handling in PHP.

---
+++ error.php
@@ -0,0 +1,158 @@
+<?php
+/*
+ * error.php:
+ * Error handling apparatus for PHP programs.
+ * 
+ * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
+ * Email: chris@mysociety.org; WWW: http://www.mysociety.org/
+ *
+ * $Id: error.php,v 1.1 2005-01-10 15:37:20 chris Exp $
+ * 
+ */
+
+/*
+ * PHP error handling is, you will not be surprised to hear, completely
+ * amateurish. It doesn't have exceptions and it doesn't have nonlocal jumps
+ * either, so we can't emulate exceptions. (Exception: PHP 5 has exceptions,
+ * but we're not using that.)
+ * 
+ * There is the notion of an "error handler", which is a function called when
+ * an "error" occurs (generated either by PHP or raised by code via the
+ * trigger_error function. Note that trigger_error and the error handler
+ * functions are not a control flow mechanism -- the default behaviour is for
+ * the error handler to do its stuff, and then return control to the calling
+ * code. Observe that this is exactly like handling the error, except that it
+ * lacks the key concepts of "error" and "handling".
+ *
+ * We adapt the error handling apparatus as follows:
+ * 
+ *   - Install a default error handler which calls an error logging handler,
+ *     and an error display handler, and then aborts.
+ *
+ *   - By the default error logging handler takes steps to dump the error in
+ *     the proper server error log (PHP makes this quite hard, as you will
+ *     see below); the default error display handler does nothing.
+ * 
+ *   - The user may override either handler using our own functions.
+ */
+
+
+/*
+ * Implementation.
+ */
+
+/* err ERROR
+ * Report the given ERROR and abort. */
+function err($err) {
+    trigger_error($err, E_USER_ERROR);
+    exit(1); /* NOTREACHED */
+}
+
+/* err_log_webserver NUMBER STRING FILE LINE CONTEXT
+ * Log the error with the given parameters, trying to arrange that it reach the
+ * appropriate web server error log. */
+function err_log_webserver($num, $str, $file, $line, $context) {
+    /* Apache (and perhaps other webservers) logs errors preceded by a tag
+     * giving the time and "severity" of the error. The time is in the format
+     * "[%a %b %d %H:%M:%S %Y]", and the "severity" is one of "[error]",
+     * "[warning]" or "[notice]". */
+
+    $prefix = '';
+    
+    /* Time. */
+/*
+    $prefix .= strftime('[%a %b %d %H:%M:%S %Y]') . " ";
+*/
+    /* Severity. */
+/*
+    if ($num & (E_WARNING | E_CORE_WARNING | E_USER_WARNING))
+        $prefix .= '[warning]';
+    else if ($num & (E_NOTICE | E_USER_NOTICE))
+        $prefix .= '[notice]';
+    else
+        $prefix .= '[error]';
+    $prefix .= ' ';
+*/
+
+    /* File/line of error. */
+    $prefix .= "$file:$line: ";
+
+    foreach (explode("\n", $str) as $line) {
+        /* We have to use error_log here because printing to php://stderr is
+         * broken in both FastCGI and the mod_php implementations. See
+         * http://bugs.php.net/bug.php?id=31472 for the FCGI case. */
+        error_log($prefix . $line);
+    }
+}
+
+$err_handler_log = 'err_log_webserver';
+$err_handler_display = null;
+
+/* err_global_handler NUMBER STRING FILE LINE CONTEXT
+ * Handler for all categories of errors. */
+function err_global_handler($num, $str, $file, $line, $context) {
+    global $err_handler_log;
+    global $err_handler_display;
+    if (isset($err_handler_log))
+        $err_handler_log($num, $str, $file, $line, $context);
+    if (isset($err_handler_display))
+        $err_handler_display($num, $str, $file, $line, $context);
+    exit(1);
+}
+
+/* err_set_handler_log FUNCTION
+ * Set the log handler to FUNCTION (a string naming a function). It should take
+ * arguments as for the set_error_handler. FUNCTION may be null to turn off
+ * error logging. */
+function err_set_handler_log($func) {
+    if (isset($func) && !function_exists($func))
+        err("err_set_log_handler: called with name of nonexistent function '$func'");
+    $err_handler_log = $func;
+}
+
+/* err_set_handler_display FUNCTION
+ * Set the display handler to FUNCTION (a string naming a function). It should
+ * take arguments as for the set_error_handler. FUNCTION may be null to turn
+ * off error logging. */
+function err_set_handler_display($func) {
+    if (isset($func) && !function_exists($func))
+        err("err_set_display_handler: called with name of nonexistent function '$func'");
+    $err_handler_display = $func;
+}
+
+/* 
+ * Now, make some brute-force changes to our configuration, so that
+ * error-handling behaves sanely.
+ */
+
+/* Never display errors to user. */
+ini_set('display_errors', 'Off');
+ini_set('display_startup_errors', 'Off');
+
+/* Log errors; our error handler will still be called for errors raised by our
+ * code, but this means that we still get to find out about PHP internal errors
+ * such as compilation problems. */
+ini_set('log_errors', 'On');
+
+/* But do not place any arbitrary limits on the length of errors logged by
+ * error_log. */
+ini_set('log_errors_max_len', '0');
+
+/* And try, as hard as possible, to make logged errors go to the server's
+ * error log. */
+ini_set('error_log', null);
+
+/* Trap all errors. */
+ini_set('ignore_repeated_errors', 'Off');
+
+/* Don't put HTML tags in error messages. */
+ini_set('html_errors', 'Off');
+
+/* Ask for reporting of *all* errors. */
+error_reporting(E_ALL);
+
+/* Finally, set our error handler to be the default. Most classes of errors
+ * cannot be trapped by a user error-handler, of course. */
+set_error_handler('err_global_handler');
+
+?>
