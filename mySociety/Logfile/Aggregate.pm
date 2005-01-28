270ec565151c7f6d7b70a3e818c731e9fc596cf7
chris <chris>
2005-01-28 19:45:23 +0000

    Implement search-by-time.

--- mySociety/Logfile/Aggregate.pm
+++ mySociety/Logfile/Aggregate.pm
@@ -6,7 +6,7 @@
 # Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 # Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: Aggregate.pm,v 1.1 2005-01-28 19:05:12 chris Exp $
+# $Id: Aggregate.pm,v 1.2 2005-01-28 19:45:23 chris Exp $
 #
 
 package mySociety::Logfile::Aggregate;
@@ -41,7 +41,7 @@ sub _update ($) {
             $f = 1;
         }
         
-        if ($f || $self->{files}->{$n}->{generation} != $self->{files}->{l}->generation()) {
+        if ($f || $self->{files}->{$n}->{generation} != $self->{files}->{$n}->{l}->generation()) {
             $self->{files}->{$n}->{begin} = $self->{files}->{$n}->{l}->time($self->{files}->{$n}->{l}->firstline());
             $self->{files}->{$n}->{generation} = $self->{files}->{$n}->{l}->generation();
             ++$N;
@@ -52,6 +52,11 @@ sub _update ($) {
         # Make a list of the logfile objects themselves.
         $self->{order} = [
                 map { $self->{files}->{$_}->{l} }
+                    sort { $self->{files}->{$a}->{begin} <=> $self->{files}->{$b}->{begin} }
+                        keys %{$self->{files}}
+            ];
+        $self->{begin} = [
+                map { $self->{files}->{$_}->{begin} }
                     sort { $self->{files}->{$a}->{begin} <=> $self->{files}->{$b}->{begin} }
                         keys %{$self->{files}}
             ];
@@ -76,17 +81,17 @@ sub new ($$$) {
 
 sub getline ($$) {
     my ($self, $o) = @_;
+    $self->_update();
     return $self->{order}->[$o->[0]]->getline($o->[1]);
 }
 
 sub firstline ($) {
     my ($self) = @_;
-    my $f = $self->{files}->{$self->{order}->[0]};
     return [ 0, $self->{order}->[0]->firstline() ];
 }
 
 sub lastline ($) {
-    my $f = $self->{files}->{$self->{order}->[-1]};
+    my ($self) = @_;
     return [ @{$self->{order}} - 1, $self->{order}->[-1]->lastline() ];
 }
 
@@ -111,6 +116,39 @@ sub prevline ($$) {
 sub parse ($$) {
     my ($self, $line) = @_;
     return $self->{order}->[0]->parse($line); # XXX assume that parsing depends only on contents of line
+}
+
+sub findtime ($$) {
+    my ($self, $time) = @_;
+    # Find the first logfile in which the right line could lie, then search
+    # within it.
+    my ($il, $ih) = (0, @{$self->{order}} - 1);
+
+    my $tl = $self->{begin}->[$il];
+    my $th = $self->{begin}->[$ih];
+
+    my $o;
+    if ($th < $time) {
+        $o = [ $ih, $self->{order}->[$ih]->findtime($time) ];
+        return defined($o->[1]) ? $o : undef;
+    }
+    
+    while ($ih > $il + 1) {
+        my $i = int(($il + $ih) / 2);
+        my $t = $self->{begin}->[$i];
+        if ($t > $time) {
+            $ih = $i;
+        } else {
+            $il = $i;
+        }
+    }
+
+    do {
+        $o = [ $il, $self->{order}->[$il]->findtime($time) ];
+        ++$il;
+    } while ($il < @{$self->{order}} && !defined($o->[1]));
+
+    return defined($o->[1]) ? $o : undef;
 }
 
 1;
