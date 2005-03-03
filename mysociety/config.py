f39106fd7da26b5e926e51a38182eee06a710c40
francis <francis>
2005-03-03 12:58:32 +0000

    Python config file reader. Wiring up poster CGI.

---
+++ mysociety/config.py
@@ -0,0 +1,152 @@
+# config.py:
+# Very simple config parser. Our config files are sort of cod-PHP.
+#
+# Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
+# Email: francis@mysociety.org; WWW: http://www.mysociety.org/
+#
+# $Id: config.py,v 1.1 2005-03-03 12:58:32 francis Exp $
+#
+
+"""
+Parse config files (written in a sort of cod-php, using
+    define(OPTION_VALUE_NAME, "value of option");
+to define individual elements.
+
+Example use:
+    mysociety.config.set_file('../conf/general');
+    opt = mysociety.config.get('CONFIG_VARIABLE', DEFAULT_VALUE);
+"""
+
+import re
+import os
+
+def find_php():
+    """find_php() -> php_binary
+
+       Try to locate the PHP binary in various sensible places.
+    """
+    path = os.getenv("PATH") or '/bin:/usr/bin';
+    paths = path.split(':')
+    for dir in path.split(':') + \
+        ['/usr/local/bin', '/usr/bin', '/software/bin', '/opt/bin', '/opt/php/bin']:
+        for name in ['php4', 'php', 'php4-cgi', 'php-cgi']:
+            if os.path.isfile('%s/%s' % (dir, name)):
+                return '%s/%s' % (dir,name)
+    raise Exception, "unable to locate PHP binary, needed to read config file";
+
+php_path = None
+def read_config(f):
+    """read_config(FILE) ->
+
+       Read configuration from FILE, which should be the name of a PHP config
+       file.  This is parsed by PHP, and any defines with names beginning
+       "OPTION_" are extracted as config values.
+    """
+
+    # We need to find the PHP binary.
+    global php_path
+    if not php_path:
+        php_path = find_php()
+
+    # Delete everything from the environment other than our special
+    # variable to give PHP the config file name. We don't want PHP to pick
+    # up other information from our environment and turn into an FCGI
+    # server or something.
+
+    # Just copying os.environ doesn't cause the correct unsetenvs and putenvs
+    # to be called, so instead we have to explicitly store it in store_environ
+    store_environ = {}
+    for k in os.environ.keys():
+        store_environ[k] = os.environ[k]
+        del os.environ[k]
+    os.environ['MYSOCIETY_CONFIG_FILE_PATH'] = f
+    (child_stdin, child_stdout, child_stderr) = os.popen3(php_path)
+    for k,v in store_environ.iteritems():
+        os.environ[k] = v
+
+    print >>child_stdin, """
+<?php
+print "<p>PHP!!!";
+print_r($_ENV);
+#print getcwd();
+require(getenv("MYSOCIETY_CONFIG_FILE_PATH"));
+$a = get_defined_constants();
+print "start_of_options\n";
+foreach ($a as $k => $v) {
+    if (preg_match("/^OPTION_/", $k)) {
+        print substr($k, 7); /* strip off "OPTION_" */
+        print "\0";
+        print $v;
+        print "\0";
+    }
+}
+?>"""
+    child_stdin.close()
+
+    # skip any header material
+    line = True
+    while line:
+        line = child_stdout.readline()
+        if line == "start_of_options\n":
+            break
+    else:
+        raise Exception, "%s: %s: failed to read options" % (php_path, f)
+
+    # read remainder
+    buf = ''.join(child_stdout.readlines())
+    child_stdout.close()
+    vals = buf.split('\0'); # option values may be empty
+    vals.pop()  # The buffer ends "\0" so there's always a trailing empty value
+                # at the end of the buffer. I love perl! Perl is my friend!
+
+    if len(vals) % 2:
+        raise Exception, "%s: %s: bad option output from subprocess" % (php_path, f)
+
+    config = {}
+    for i in range(len(vals) / 2):
+        config[vals[i*2]] = vals[i*2+1]
+    config["CONFIG_FILE_NAME"] = f
+    return config
+
+main_config_filename = None
+def set_file(filename):
+    """set_file FILENAME
+       Sets the default configuration file, used by mySociety::Config::get.
+    """
+    
+    global main_config_filename
+    main_config_filename = filename
+
+cached_configs = {}
+def load_default():
+    """load_default
+       Loads and caches default config file, as set with set_file.  This
+       function is implicitly called by get and get_all.
+    """
+
+    global main_config_filename, cached_configs
+
+    filename = main_config_filename
+    if not filename:
+        raise Exception, "Please call mysociety::config::set_file to specify config file" 
+
+    if not filename in cached_configs:
+        cached_configs[filename] = read_config(filename)
+    return cached_configs[filename]
+
+def get (key, default = None):
+    """get KEY [DEFAULT]
+       Returns the constants set for KEY from the configuration file specified
+       in set_config_file. The file is automatically loaded and cached. An
+       exception is thrown if the value isn't present and no DEFAULT is
+       specified.
+    """
+
+    config = load_default()
+    
+    if key in config:
+        return config[key]
+    elif default:
+        return default
+    else:
+        raise Exception, "No value for '%s' in '%s', and no default specified" % (key, config['CONFIG_FILE_NAME'])
