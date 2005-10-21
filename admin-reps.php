0426cfa6b30a567ca7207bdd3bfb6f26a974e124
francis <francis>
2005-10-21 11:07:35 +0000

    Add search links for rep name and "tel/fax"

--- admin-reps.php
+++ admin-reps.php
@@ -5,7 +5,7 @@
  * Copyright (c) 2004 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org. WWW: http://www.mysociety.org
  *
- * $Id: admin-reps.php,v 1.27 2005-10-21 10:32:33 francis Exp $
+ * $Id: admin-reps.php,v 1.28 2005-10-21 11:07:35 francis Exp $
  * 
  */
 
@@ -264,6 +264,15 @@ class ADMIN_PAGE_REPS {
                     '<br><a href="'.$self_link.'&ds_va_id='
                     . $vainfo['parent_area_id'] . '">... or edit Democratic Services for this council</a>');
             }
+            $search_links = "Search for: ";
+            foreach (array(
+                "tel ". $repinfo['name'],
+                "fax ". $repinfo['name'],
+                "tel ". $repinfo['name'] . " " . $vainfo['rep_name'],
+                "fax ". $repinfo['name'] . " " . $vainfo['rep_name']
+                ) as $searchq) 
+                $search_links .= "<a href=\"http://search.yahoo.com/search?p=".htmlspecialchars($searchq)."\"> ".htmlspecialchars($searchq)."</a> | ";
+            $form->addElement('static', 'newlink', null, $search_links);
     
             $form->addElement('header', '', 'Historical Changes');
             $html = "<table border=1>";
