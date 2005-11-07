c0a4a21c2d5be8a394bd5ee6ef92c628baa1ea20
matthew <matthew>
2005-11-07 18:24:24 +0000

    Don't output HTML error if generating a JS file.

--- gaze.php
+++ gaze.php
@@ -8,7 +8,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * WWW: http://www.mysociety.org
  *
- * $Id: gaze.php,v 1.9 2005-10-27 14:59:14 francis Exp $
+ * $Id: gaze.php,v 1.10 2005-11-07 18:24:24 matthew Exp $
  *
  */
 
@@ -25,8 +25,8 @@ function gaze_get_error($e) {
 
 /* gaze_check_error R
  * If R indicates failure, displays error message and stops procesing. */
-function gaze_check_error($data) {
-    if ($error_message = gaze_get_error($data))
+function gaze_check_error($data, $output_error = true) {
+    if ($error_message = gaze_get_error($data) && $output_error)
         err($error_message);
 }
 
