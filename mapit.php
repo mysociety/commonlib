6aad495a3411c58e8ad085a022880b6900efaadf
francis <francis>
2005-12-07 16:42:14 +0000

    Store cobrand type and cocode (an extra code the cobranding organisation can use for whatever they like) with message. Pass cocode round from initial link to stick with message. Use cobrand field in message for URL in confirmation emails. Test code for cobranding.

--- mapit.php
+++ mapit.php
@@ -8,7 +8,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * WWW: http://www.mysociety.org
  *
- * $Id: mapit.php,v 1.31 2005-12-02 18:36:32 francis Exp $
+ * $Id: mapit.php,v 1.32 2005-12-07 16:42:16 francis Exp $
  *
  */
 
