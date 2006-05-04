b31cfa83fcf25164a8a8b7716a6084d1c0c0fc16
chris <chris>
2006-05-04 11:42:08 +0000

    Separate out invoked_url and new_url.

---
+++ urls.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * urls.php:
+ * URL functions.
+ * 
+ * Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
+ * Email: chris@mysociety.org; WWW: http://www.mysociety.org/
+ *
+ * $Id: urls.php,v 1.1 2006-05-04 11:42:08 chris Exp $
+ * 
+ */
+
+/* invoked_url
+ * Return the URL under which the script was invoked. The port is specified
+ * only if it is not the default (i.e. 80 for HTTP and 443 for HTTPS). */
+function invoked_url() {
+    $url = 'http';
+    $ssl = FALSE;
+    if (array_key_exists('SSL', $_SERVER)) {
+        $url .= "s";
+        $ssl = TRUE;
+    }
+    $url .= "://" . $_SERVER['HTTP_HOST'];
+
+    if ((!$ssl && $_SERVER['SERVER_PORT'] != 80)
+        || ($ssl && $_SERVER['SERVER_PORT'] != 443))
+        $url .= ":" . $_SERVER['SERVER_PORT'];
+
+    $url .= preg_replace("/\?.*/", "", $_SERVER['REQUEST_URI']);
+
+    return $url;
+}
+
+/* new_url PAGE RETAIN [PARAM VALUE ...]
+ * Return a new URL for PAGE with added parameters. If RETAIN is true, then all
+ * of the parameters with which the page was originally invoked will be
+ * retained in the original URL; additionally, any PARAM VALUE pairs will be
+ * added. If a PARAM is specified it overrides any retained parameter value; if
+ * a VALUE is null, any retained PARAM is removed. If a VALUE is an array,
+ * multiple URL parameters will be added. If PAGE is null the URL under which
+ * this page was invoked is used. */
+function new_url($page, $retain) {
+    if (!isset($page))
+        $page = invoked_url();
+    $url = "$page";
+
+    $params = array();
+    if ($retain)
+        /* GET takes priority over POST. This isn't the usual behaviour but is
+         * consistent with other bits of the code (see fyr/phplib/forms.php) */
+        $params = array_merge($_POST, $_GET);
+
+    if (func_num_args() > 2) {
+        if ((func_num_args() % 2) != 0)
+            die("call to new_url with odd number of arguments");
+        for ($i = 2; $i < func_num_args(); $i += 2) {
+            $k = func_get_arg($i);
+            $v = func_get_arg($i + 1);
+            if (array_key_exists($k, $params))
+                unset($params[$k]);
+            $params[func_get_arg($i)] = func_get_arg($i + 1);
+        }
+    }
+    
+    if (count($params) > 0) {
+        $keyvalpairs = array();
+        foreach ($params as $key => $val) {
+            if (is_array($val)) {
+                for ($i = 0; $i < count($val); ++$i)
+                    $keyvalpairs[] = urlencode($key) . '=' . urlencode($val[$i]);
+            } elseif ($val)
+                $keyvalpairs[] = urlencode($key) . '=' . urlencode($val);
+        }
+        $url .= '?' . join('&', $keyvalpairs);
+    }
+
+    return $url;
+}
+
+?>
--- utility.php
+++ utility.php
@@ -7,7 +7,7 @@
  * Mainly: Copyright (c) 2003-2004, FaxYourMP Ltd 
  * Parts are: Copyright (c) 2004 UK Citizens Online Democracy
  *
- * $Id: utility.php,v 1.68 2006-04-17 14:06:40 matthew Exp $
+ * $Id: utility.php,v 1.69 2006-05-04 11:42:08 chris Exp $
  * 
  */
 
@@ -45,6 +45,11 @@ set_magic_quotes_runtime(0);
 /*
  * Actually useful functions begin below here.
  */
+
+/* XXX These functions used to be in utility.php, but have been separated so
+ * that they can be called from TWFY (which has namespace clashes with the
+ * functions in this file). So we include it here for compatibility. */
+require_once('urls.php');
 
 /* debug HEADER TEXT [VARIABLE]
  * Print, to the page, a debugging variable, if a debug=... parameter is
@@ -416,27 +421,6 @@ function debug_comment_timestamp($extra = null) {
     debug_timestamp(true, $extra);
 }
 
-/* invoked_url
- * Return the URL under which the script was invoked. The port is specified
- * only if it is not the default (i.e. 80 for HTTP and 443 for HTTPS). */
-function invoked_url() {
-    $url = 'http';
-    $ssl = FALSE;
-    if (array_key_exists('SSL', $_SERVER)) {
-        $url .= "s";
-        $ssl = TRUE;
-    }
-    $url .= "://" . $_SERVER['HTTP_HOST'];
-
-    if ((!$ssl && $_SERVER['SERVER_PORT'] != 80)
-        || ($ssl && $_SERVER['SERVER_PORT'] != 443))
-        $url .= ":" . $_SERVER['SERVER_PORT'];
-
-    $url .= preg_replace("/\?.*/", "", $_SERVER['REQUEST_URI']);
-
-    return $url;
-}
-
 /* javascript_focus_set FORM ELEMENT
  * Return a bit of JavaScript which will set the user's input focus to the
  * input element of the given FORM (id) and ELEMENT (name). */
@@ -452,51 +436,6 @@ function check_is_valid_regexp($regex) {
     return ($result !== FALSE);
 }
 
-/* new_url PAGE RETAIN [PARAM VALUE ...]
- * Return a new URL for PAGE with added parameters. If RETAIN is true, then all
- * of the parameters with which the page was originally invoked will be
- * retained in the original URL; additionally, any PARAM VALUE pairs will be
- * added. If a PARAM is specified it overrides any retained parameter value; if
- * a VALUE is null, any retained PARAM is removed. If a VALUE is an array,
- * multiple URL parameters will be added. If PAGE is null the URL under which
- * this page was invoked is used. */
-function new_url($page, $retain) {
-    if (!isset($page))
-        $page = invoked_url();
-    $url = "$page";
-
-    $params = array();
-    if ($retain)
-        /* GET takes priority over POST. This isn't the usual behaviour but is
-         * consistent with other bits of the code (see fyr/phplib/forms.php) */
-        $params = array_merge($_POST, $_GET);
-
-    if (func_num_args() > 2) {
-        if ((func_num_args() % 2) != 0)
-            die("call to new_url with odd number of arguments");
-        for ($i = 2; $i < func_num_args(); $i += 2) {
-            $k = func_get_arg($i);
-            $v = func_get_arg($i + 1);
-            if (array_key_exists($k, $params))
-                unset($params[$k]);
-            $params[func_get_arg($i)] = func_get_arg($i + 1);
-        }
-    }
-    
-    if (count($params) > 0) {
-        $keyvalpairs = array();
-        foreach ($params as $key => $val) {
-            if (is_array($val)) {
-                for ($i = 0; $i < count($val); ++$i)
-                    $keyvalpairs[] = urlencode($key) . '=' . urlencode($val[$i]);
-            } elseif ($val)
-                $keyvalpairs[] = urlencode($key) . '=' . urlencode($val);
-        }
-        $url .= '?' . join('&', $keyvalpairs);
-    }
-
-    return $url;
-}
 
 /* http_auth_user
  * Return the user name authenticated by HTTP, or *unknown* if none.
