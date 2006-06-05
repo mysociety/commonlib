9b398d1d28cb36940fd933bc9b3c5d7cb9346ffd
chris <chris>
2006-06-05 18:50:33 +0000

    Split debug_ functions out.

---
+++ debug.php
@@ -0,0 +1,106 @@
+<?php
+/*
+ * debug.php:
+ * Debugging functions.
+ * 
+ * Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
+ * Email: chris@mysociety.org; WWW: http://www.mysociety.org/
+ *
+ * $Id: debug.php,v 1.1 2006-06-05 18:50:33 chris Exp $
+ * 
+ */
+
+/* debug HEADER TEXT [VARIABLE]
+ * Print, to the page, a debugging variable, if a debug=... parameter is
+ * present. The message is prefixed with the HEADER and consists of the passed
+ * TEXT and an optional (perhaps array or class) VARIABLE which, if present, is
+ * also dumped to the page. Display of items is dependent on the integer value
+ * of the debug query variable and the passed HEADER, according to the table in
+ * $levels below. */
+function debug ($header, $text="", $complex_variable=null) {
+
+	// We set ?debug=n in the URL.
+	// n is a number from (currently) 1 to 4.
+	// This sets what amount of debug information is shown.
+	// For level '1' we show anything that is passed to this function
+	// with a $header in $levels[1].
+	// For level '2', anything with a $header in $levels[1] AND $levels[2].
+	// Level '4' shows everything.
+    // $complex_variable is dumped in full, so you can put arrays/hashes here
+	
+	//$debug_level = get_http_var("debug");  // disabled - information revealing security hole
+
+    $debug_level = OPTION_PHP_DEBUG_LEVEL;
+	
+	if ($debug_level != '') {
+	
+		// Set which level shows which types of debug info.
+		$levels = array (
+			1 => array ('FRONTEND', 'WARNING', 'MAPIT', 'DADEM', 'QUEUE', 'TIMESTAMP'),
+			2 => array ('MAPITRESULT', 'DADEMRESULT', 'RATTY'), 
+			3 => array ('XMLRPC', 'RABX', 'RATTYRESULT'),
+			4 => array ('RABXWIRE', 'SERIALIZE'),
+		);
+	
+		// Store which headers we are allowed to show.
+		$allowed_headers = array();
+		
+		if ($debug_level > count($levels)) {
+			$max_level_to_show = count($levels);
+		} else {
+			$max_level_to_show = $debug_level;
+		}
+		
+		for ($n = 1; $n <= $max_level_to_show; $n++) {
+			$allowed_headers = array_merge ($allowed_headers, $levels[$n] );
+		}
+		
+		// If we can show this header, then, er, show it.
+		if ( in_array($header, $allowed_headers) || $debug_level >= 4) {
+            	
+			print "<p><span style=\"color:#039;\"><strong>$header</strong></span> $text";
+            if (isset($complex_variable)) {
+                print "</p><p>";
+                vardump($complex_variable);
+            }
+            print "</p>\n";	
+		}
+	}
+}
+
+/* vardump VARIABLE
+* Dump VARIABLE to the page, properly escaped and wrapped in <pre> tags. */
+function vardump($blah) {
+   /* Miserable. We need to encode entities in the output, which means messing
+    * about with output buffering. */
+   ob_start();
+   var_dump($blah);
+   $d = ob_get_contents();
+   ob_end_clean();
+   print "<pre>" . htmlspecialchars($d, ENT_QUOTES, 'UTF-8') . "</pre>";
+}
+
+/* debug_timestamp
+ * Output a timestamp since the page was started. */
+$timestamp_last = $timestamp_start = getmicrotime();
+function debug_timestamp($comment = false, $extra = null) {
+    global $timestamp_last, $timestamp_start;
+    $t = getmicrotime();
+    if ($comment)
+        printf("<!--\n   %s\n    %f seconds since start\n    %f seconds since last\n-->",
+                is_null($extra) ? 'TIMESTAMP' : $extra, $t - $timestamp_start, $t - $timestamp_last);
+    else
+        debug("TIMESTAMP", sprintf("%f seconds since start; %f seconds since last",
+                $t - $timestamp_start, $t - $timestamp_last));
+    $timestamp_last = $t;
+}
+
+/* debug_comment_timestamp [NOTE]
+ * As debug_timestamp, but print the timestamp in an HTML comment, whether or
+ * not the debug flags are set. If specified, NOTE will be printed in the
+ * comment. */
+function debug_comment_timestamp($extra = null) {
+    debug_timestamp(true, $extra);
+}
+
+?>
--- utility.php
+++ utility.php
@@ -7,7 +7,7 @@
  * Mainly: Copyright (c) 2003-2004, FaxYourMP Ltd 
  * Parts are: Copyright (c) 2004 UK Citizens Online Democracy
  *
- * $Id: utility.php,v 1.72 2006-05-04 12:15:16 chris Exp $
+ * $Id: utility.php,v 1.73 2006-06-05 18:50:33 chris Exp $
  * 
  */
 
@@ -50,81 +50,8 @@ set_magic_quotes_runtime(0);
  * that they can be called from TWFY (which has namespace clashes with the
  * functions in this file). So we include it here for compatibility. */
 require_once('urls.php');
-
 require_once('random.php');
-
-/* debug HEADER TEXT [VARIABLE]
- * Print, to the page, a debugging variable, if a debug=... parameter is
- * present. The message is prefixed with the HEADER and consists of the passed
- * TEXT and an optional (perhaps array or class) VARIABLE which, if present, is
- * also dumped to the page. Display of items is dependent on the integer value
- * of the debug query variable and the passed HEADER, according to the table in
- * $levels below. */
-function debug ($header, $text="", $complex_variable=null) {
-
-	// We set ?debug=n in the URL.
-	// n is a number from (currently) 1 to 4.
-	// This sets what amount of debug information is shown.
-	// For level '1' we show anything that is passed to this function
-	// with a $header in $levels[1].
-	// For level '2', anything with a $header in $levels[1] AND $levels[2].
-	// Level '4' shows everything.
-    // $complex_variable is dumped in full, so you can put arrays/hashes here
-	
-	//$debug_level = get_http_var("debug");  // disabled - information revealing security hole
-
-    $debug_level = OPTION_PHP_DEBUG_LEVEL;
-	
-	if ($debug_level != '') {
-	
-		// Set which level shows which types of debug info.
-		$levels = array (
-			1 => array ('FRONTEND', 'WARNING', 'MAPIT', 'DADEM', 'QUEUE', 'TIMESTAMP'),
-			2 => array ('MAPITRESULT', 'DADEMRESULT', 'RATTY'), 
-			3 => array ('XMLRPC', 'RABX', 'RATTYRESULT'),
-			4 => array ('RABXWIRE', 'SERIALIZE'),
-		);
-	
-		// Store which headers we are allowed to show.
-		$allowed_headers = array();
-		
-		if ($debug_level > count($levels)) {
-			$max_level_to_show = count($levels);
-		} else {
-			$max_level_to_show = $debug_level;
-		}
-		
-		for ($n = 1; $n <= $max_level_to_show; $n++) {
-			$allowed_headers = array_merge ($allowed_headers, $levels[$n] );
-		}
-		
-		// If we can show this header, then, er, show it.
-		if ( in_array($header, $allowed_headers) || $debug_level >= 4) {
-            	
-			print "<p><span style=\"color:#039;\"><strong>$header</strong></span> $text";
-            if (isset($complex_variable)) {
-                print "</p><p>";
-                vardump($complex_variable);
-            }
-            print "</p>\n";	
-		}
-	}
-}
-
-
-/* vardump VARIABLE
- * Dump VARIABLE to the page, properly escaped and wrapped in <pre> tags. */
-function vardump($blah) {
-    /* Miserable. We need to encode entities in the output, which means messing
-     * about with output buffering. */
-    ob_start();
-    var_dump($blah);
-    $d = ob_get_contents();
-    ob_end_clean();
-    print "<pre>" . htmlspecialchars($d, ENT_QUOTES, 'UTF-8') . "</pre>";
-}
-
-
+require_once('debug.php');
 
 /* validate_email STRING
  * Return TRUE if the passed STRING may be a valid email address. 
@@ -139,7 +66,6 @@ function validate_email ($address) {
     else
         return false;
 }
-
 
 /* validate_postcode POSTCODE
  * Return true is POSTCODE is in the proper format for a UK postcode. Does not
@@ -172,8 +98,7 @@ function validate_postcode ($postcode) {
 }
 
 /* validate_partial_postcode PARTIAL_POSTCODE
- * Return true is POSTCODE is the first part of a UK postcode.  e.g. WC1.
- */
+ * Return true is POSTCODE is the first part of a UK postcode.  e.g. WC1. */
 function validate_partial_postcode ($postcode) {
     // Our test postcode
     if (preg_match("/^zz9/i", $postcode))
@@ -402,29 +327,6 @@ function make_plural($number, $singular, $plural='') {
 	return $singular . 's';
 }
 
-/* debug_timestamp
- * Output a timestamp since the page was started. */
-$timestamp_last = $timestamp_start = getmicrotime();
-function debug_timestamp($comment = false, $extra = null) {
-    global $timestamp_last, $timestamp_start;
-    $t = getmicrotime();
-    if ($comment)
-        printf("<!--\n   %s\n    %f seconds since start\n    %f seconds since last\n-->",
-                is_null($extra) ? 'TIMESTAMP' : $extra, $t - $timestamp_start, $t - $timestamp_last);
-    else
-        debug("TIMESTAMP", sprintf("%f seconds since start; %f seconds since last",
-                $t - $timestamp_start, $t - $timestamp_last));
-    $timestamp_last = $t;
-}
-
-/* debug_comment_timestamp [NOTE]
- * As debug_timestamp, but print the timestamp in an HTML comment, whether or
- * not the debug flags are set. If specified, NOTE will be printed in the
- * comment. */
-function debug_comment_timestamp($extra = null) {
-    debug_timestamp(true, $extra);
-}
-
 /* javascript_focus_set FORM ELEMENT
  * Return a bit of JavaScript which will set the user's input focus to the
  * input element of the given FORM (id) and ELEMENT (name). */
@@ -439,7 +341,6 @@ function check_is_valid_regexp($regex) {
     $result = preg_match("/" . str_replace("/", "\/", $regex) .  "/", "");
     return ($result !== FALSE);
 }
-
 
 /* http_auth_user
  * Return the user name authenticated by HTTP, or *unknown* if none.
