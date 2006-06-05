1c3fd588a5c79d6e312bbebb83e93d45dcdadb54
chris <chris>
2006-06-05 18:52:59 +0000

    Indentation; remove out-of-date comment.

--- debug.php
+++ debug.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
  * Email: chris@mysociety.org; WWW: http://www.mysociety.org/
  *
- * $Id: debug.php,v 1.2 2006-06-05 18:51:26 chris Exp $
+ * $Id: debug.php,v 1.3 2006-06-05 18:52:59 chris Exp $
  * 
  */
 
@@ -18,54 +18,38 @@
  * of the debug query variable and the passed HEADER, according to the table in
  * $levels below. */
 function debug ($header, $text="", $complex_variable=null) {
-
-	// We set ?debug=n in the URL.
-	// n is a number from (currently) 1 to 4.
-	// This sets what amount of debug information is shown.
-	// For level '1' we show anything that is passed to this function
-	// with a $header in $levels[1].
-	// For level '2', anything with a $header in $levels[1] AND $levels[2].
-	// Level '4' shows everything.
-    // $complex_variable is dumped in full, so you can put arrays/hashes here
-	
-	//$debug_level = get_http_var("debug");  // disabled - information revealing security hole
-
     $debug_level = OPTION_PHP_DEBUG_LEVEL;
-	
-	if ($debug_level != '') {
-	
-		// Set which level shows which types of debug info.
-		$levels = array (
-			1 => array ('FRONTEND', 'WARNING', 'MAPIT', 'DADEM', 'QUEUE', 'TIMESTAMP'),
-			2 => array ('MAPITRESULT', 'DADEMRESULT', 'RATTY'), 
-			3 => array ('XMLRPC', 'RABX', 'RATTYRESULT'),
-			4 => array ('RABXWIRE', 'SERIALIZE'),
-		);
-	
-		// Store which headers we are allowed to show.
-		$allowed_headers = array();
-		
-		if ($debug_level > count($levels)) {
-			$max_level_to_show = count($levels);
-		} else {
-			$max_level_to_show = $debug_level;
-		}
-		
-		for ($n = 1; $n <= $max_level_to_show; $n++) {
-			$allowed_headers = array_merge ($allowed_headers, $levels[$n] );
-		}
-		
-		// If we can show this header, then, er, show it.
-		if ( in_array($header, $allowed_headers) || $debug_level >= 4) {
-            	
-			print "<p><span style=\"color:#039;\"><strong>$header</strong></span> $text";
+
+    if ($debug_level != '') {
+        // Set which level shows which types of debug info.
+        $levels = array (
+                1 => array ('FRONTEND', 'WARNING', 'MAPIT', 'DADEM', 'QUEUE', 'TIMESTAMP'),
+                2 => array ('MAPITRESULT', 'DADEMRESULT', 'RATTY'), 
+                3 => array ('XMLRPC', 'RABX', 'RATTYRESULT'),
+                4 => array ('RABXWIRE', 'SERIALIZE'),
+                );
+
+        // Store which headers we are allowed to show.
+        $allowed_headers = array();
+
+        if ($debug_level > count($levels))
+            $max_level_to_show = count($levels);
+        else
+            $max_level_to_show = $debug_level;
+
+        for ($n = 1; $n <= $max_level_to_show; $n++)
+            $allowed_headers = array_merge($allowed_headers, $levels[$n]);
+
+        // If we can show this header, then, er, show it.
+        if (in_array($header, $allowed_headers) || $debug_level >= 4) {
+            print "<p><span style=\"color:#039;\"><strong>$header</strong></span> $text";
             if (isset($complex_variable)) {
                 print "</p><p>";
                 debug_vardump($complex_variable);
             }
             print "</p>\n";	
-		}
-	}
+        }
+    }
 }
 
 /* debug_vardump VARIABLE
