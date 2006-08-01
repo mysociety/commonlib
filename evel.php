2a4279608f7b6b67ee69b31acad66d31a2175cbb
francis <francis>
2006-08-01 14:02:41 +0000

    Give questionnaire template access to type of representative. Change message in response rate questionnaire, to make it clear it is the MPs responsibility to tell people if they change their contact details, and to mention surgeries as a worthwhile alternative contact method.

--- evel.php
+++ evel.php
@@ -8,7 +8,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * WWW: http://www.mysociety.org
  *
- * $Id: evel.php,v 1.28 2006-07-13 15:48:07 francis Exp $
+ * $Id: evel.php,v 1.29 2006-08-01 14:02:44 francis Exp $
  *
  */
 
--- gaze.php
+++ gaze.php
@@ -8,7 +8,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * WWW: http://www.mysociety.org
  *
- * $Id: gaze.php,v 1.22 2006-07-13 15:48:07 francis Exp $
+ * $Id: gaze.php,v 1.23 2006-08-01 14:02:44 francis Exp $
  *
  */
 
