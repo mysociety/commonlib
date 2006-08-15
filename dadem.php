dac35f7c7904eab346f783740bfa0e5c07ed4250
francis <francis>
2006-08-15 17:31:29 +0000

    Make fourweek stats work. Make it quicker on most message pages.

--- dadem.php
+++ dadem.php
@@ -8,7 +8,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * WWW: http://www.mysociety.org
  *
- * $Id: dadem.php,v 1.42 2006-08-01 14:02:44 francis Exp $
+ * $Id: dadem.php,v 1.43 2006-08-15 17:31:32 francis Exp $
  *
  */
 
--- evel.php
+++ evel.php
@@ -8,7 +8,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * WWW: http://www.mysociety.org
  *
- * $Id: evel.php,v 1.29 2006-08-01 14:02:44 francis Exp $
+ * $Id: evel.php,v 1.30 2006-08-15 17:31:32 francis Exp $
  *
  */
 
