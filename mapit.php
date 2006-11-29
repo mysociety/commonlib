f849c8a53a74dfbdc64b250e10ed347e2cc2755f
matthew <matthew>
2006-11-29 18:59:28 +0000

    Only 1 lineending mess-up, I'm quite impressed.

--- mapit.php
+++ mapit.php
@@ -8,7 +8,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * WWW: http://www.mysociety.org
  *
- * $Id: mapit.php,v 1.51 2006-11-29 18:55:44 matthew Exp $
+ * $Id: mapit.php,v 1.52 2006-11-29 18:59:28 matthew Exp $
  *
  */
 
