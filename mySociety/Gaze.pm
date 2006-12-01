66e2fe813cc3682dc99ba9ea5dbf984b352a52db
matthew <matthew>
2006-12-01 16:35:29 +0000

    Actually show something useful.

--- mySociety/Gaze.pm
+++ mySociety/Gaze.pm
@@ -8,7 +8,7 @@
 # Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 # WWW: http://www.mysociety.org
 #
-# $Id: Gaze.pm,v 1.24 2006-12-01 16:28:24 matthew Exp $
+# $Id: Gaze.pm,v 1.25 2006-12-01 16:35:29 matthew Exp $
 
 package mySociety::Gaze;
 
@@ -121,7 +121,7 @@ sub get_radius_containing_population ($$$;$) {
 
 =item Gaze::get_places_near LAT LON [PARAMS]
 
-  Returns a reference to an associative array of all the places within a
+  Returns a reference to an array of all the places within a
   given distance of a point expressed as LAT LON. PARAMS include: *
   DISTANCE to include all results within DISTANCE km * POPULATION to
   include all results within a circle containing at least POPULATION people
