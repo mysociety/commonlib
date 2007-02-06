b364ccdfc1690b0a30df002e4e16bbd4d6e2b81e
francis <francis>
2007-02-06 13:19:48 +0000

    Use urandom a bit more

--- random.php
+++ random.php
@@ -1,17 +1,17 @@
 <?php
 /*
  * random.php:
- * Acquire (strongly) random bytes from /dev/random.
+ * Acquire random bytes from /dev/random and /dev/urandom.
  * 
  * Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
  * Email: chris@mysociety.org; WWW: http://www.mysociety.org/
  *
- * $Id: random.php,v 1.1 2006-05-04 11:58:32 chris Exp $
+ * $Id: random.php,v 1.2 2007-02-06 13:19:49 francis Exp $
  * 
  */
 
 /* random_bytes NUM
- * Return NUM bytes of random data. */
+ * Return NUM bytes of random data from /dev/random. */
 function random_bytes($num) {
     global $random_bytes_filehandle;
     if ($num < 0)
@@ -22,6 +22,21 @@ function random_bytes($num) {
     $res = '';
     while (strlen($res) < $num)
         $res .= fread($random_bytes_filehandle, $num - strlen($res));
+    return $res;
+}
+
+/* urandom_bytes NUM
+ * Return NUM bytes of (partly pseudo) random data from /dev/urandom. */
+function urandom_bytes($num) {
+    global $urandom_bytes_filehandle;
+    if ($num < 0)
+        err("NUM must be nonnegative in urandom_bytes");
+    if (!isset($urandom_bytes_filehandle)
+        && !($urandom_bytes_filehandle = fopen("/dev/urandom", "r")))
+            err("Unable to open /dev/urandom");
+    $res = '';
+    while (strlen($res) < $num)
+        $res .= fread($urandom_bytes_filehandle, $num - strlen($res));
     return $res;
 }
 
