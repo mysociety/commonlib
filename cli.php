9cd962fd0e9a0d257a8e4838df6d0b730f0b7fb0
francis <francis>
2007-04-16 13:34:06 +0000

    Only send list of MPs we don't have emails for once a week

--- cli.php
+++ cli.php
@@ -6,12 +6,13 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * Email: chris@mysociety.org; WWW: http://www.mysociety.org/
  *
- * $Id: cli.php,v 1.4 2005-11-07 16:47:11 sandpit Exp $
+ * $Id: cli.php,v 1.5 2007-04-16 13:34:06 francis Exp $
  * 
  */
 
 $cli_program_name = preg_replace('#^.*/#', '', $argv[0]);
 $cli_is_verbose = false;
+$cli_show_warnings = true;
 
 /* verbose STRING
  * If $cli_is_verbose is true, then print STRING to standard error, followed
@@ -25,7 +26,8 @@ function verbose($str) {
 /* warning STRING
  * Print STRING to standard error, followed by a \n. */
 function warning($str) {
-    global $cli_program_name;
+    global $cli_program_name, $cli_show_warnings;
+    if (!$cli_show_warnings) return;
     fwrite(STDERR, "$cli_program_name: $str\n");
 }
 
