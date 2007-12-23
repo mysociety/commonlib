3d3f2658f88726655f41591bf1b7a8553adeb08b
francis <francis>
2007-12-23 13:44:18 +0000

    Make links in incoming and outgoing messages hyperlinks.

--- format.rb
+++ format.rb
@@ -4,7 +4,7 @@
 # Copyright (c) 2007 UK Citizens Online Democracy. All rights reserved.
 # Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: format.rb,v 1.4 2007-12-17 03:31:34 francis Exp $
+# $Id: format.rb,v 1.5 2007-12-23 13:44:19 francis Exp $
 
 module MySociety
     module Format
@@ -22,6 +22,24 @@ module MySociety
                 result = result + indent + para + "\n\n"
             end
             return result
+        end
+
+        # Returns text with obvious links made into HTML hrefs.
+        # Taken originally from phplib/utility.php and from WordPress, tweaked somewhat.
+        def Format.make_clickable(text, params = {})
+            nofollow = params[:nofollow]
+            contract = params[:contract]
+
+            ret = ' ' + text + ' '
+            ret = ret.gsub(/(https?):\/\/([^\s<>{}()]+[^\s.,<>{}()])/i, "<a href='\\1://\\2'" + (nofollow ? " rel='nofollow'" : "") + ">\\1://\\2</a>")
+            ret = ret.gsub(/(\s)www\.([a-z0-9\-]+)((?:\.[a-z0-9\-\~]+)+)((?:\/[^ <>{}()\n\r]*[^., <>{}()\n\r])?)/i,
+                        "\\1<a href='http://www.\\2\\3\\4'" + (nofollow ? " rel='nofollow'" : "") + ">www.\\2\\3\\4</a>")
+            if contract
+                ret = ret.gsub(/(<a href='[^']*'(?: rel='nofollow')?>)([^<]{40})[^<]{3,}<\/a>/, '\\1\\2...</a>')
+            end
+            ret = ret.gsub(/(\s)([a-z0-9\-_.]+)@([^,< \n\r]*[^.,< \n\r])/i, "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>")
+            ret = ret.strip
+            return ret
         end
     end
 end
