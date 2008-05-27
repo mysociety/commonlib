51de2378ef74ce9c7af701c75c327e812232ff43
francis <francis>
2008-05-27 09:14:10 +0000

    Oops, forgot this.

--- validate.rb
+++ validate.rb
@@ -4,7 +4,7 @@
 # Copyright (c) 2007 UK Citizens Online Democracy. All rights reserved.
 # Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: validate.rb,v 1.2 2007-12-17 18:30:59 francis Exp $
+# $Id: validate.rb,v 1.3 2008-05-27 09:14:10 francis Exp $
 
 module MySociety
     module Validate
@@ -44,6 +44,54 @@ module MySociety
 
             return addr =~ is_valid_address_re
         end
+
+        # For finding email addresses in a body of text.
+        # XXX Less exact than the one above, but I had problems in Ruby's
+        # regexp engine with the one above crashing it.
+        def Validate.email_find_regexp
+            return Regexp.new("(\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b)")
+        end
+
+
+        # validate_postcode POSTCODE
+        # Return true is POSTCODE is in the proper format for a UK postcode. Does not
+        # require spaces in the appropriate place.
+        def Validate.is_valid_postcode(postcode)
+            return Validate.postcode_match_internal(postcode, "^", "$")
+        end
+        def Validate.contains_postcode?(postcode)
+            return Validate.postcode_match_internal(postcode, "\\b", "\\b")
+        end
+
+        def Validate.postcode_match_internal(postcode, pre, post)
+            # Our test postcode
+            if (postcode.match("/#{pre}zz9\s*9z[zy]$/i"))
+                return true 
+            end
+            
+            # See http://www.govtalk.gov.uk/gdsc/html/noframes/PostCode-2-1-Release.htm
+            inn  = 'ABDEFGHJLNPQRSTUWXYZ'
+            fst = 'ABCDEFGHIJKLMNOPRSTUWYZ'
+            sec = 'ABCDEFGHJKLMNOPQRSTUVWXY'
+            thd = 'ABCDEFGHJKSTUW'
+            fth = 'ABEHMNPRVWXY'
+            num0 = '123456789' # Technically allowed in spec, but none exist
+            num = '0123456789'
+            nom = '0123456789'
+            gap = '\s\.'	
+
+            if (postcode.match(/#{pre}[#{fst}][#{num0}][#{gap}]*[#{nom}][#{inn}][#{inn}]#{post}/i) ||
+                postcode.match(/#{pre}[#{fst}][#{num0}][#{num}][#{gap}]*[#{nom}][#{inn}][#{inn}]#{post}/i) ||
+                postcode.match(/#{pre}[#{fst}][#{sec}][#{num}][#{gap}]*[#{nom}][#{inn}][#{inn}]#{post}/i) ||
+                postcode.match(/#{pre}[#{fst}][#{sec}][#{num0}][#{num}][#{gap}]*[#{nom}][#{inn}][#{inn}]#{post}/i) ||
+                postcode.match(/#{pre}[#{fst}][#{num0}][#{thd}][#{gap}]*[#{nom}][#{inn}][#{inn}]#{post}/i) ||
+                postcode.match(/#{pre}[#{fst}][#{sec}][#{num0}][#{fth}][#{gap}]*[#{nom}][#{inn}][#{inn}]#{post}/i))
+                return true
+            else
+                return false
+            end
+        end
+
     end
 end
 
