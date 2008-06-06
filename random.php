5001297770ac40ad0cf49d39eda1e13bcc5ba5e5
francis <francis>
2008-06-06 12:07:34 +0000

    Patch from Tom Lynn

--- random.php
+++ random.php
@@ -6,13 +6,23 @@
  * Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
  * Email: chris@mysociety.org; WWW: http://www.mysociety.org/
  *
- * $Id: random.php,v 1.2 2007-02-06 13:19:49 francis Exp $
+ * $Id: random.php,v 1.3 2008-06-06 12:07:34 francis Exp $
  * 
  */
+
+if (!file_exists('/dev/random') || !file_exists('/dev/urandom'))
+    require_once 'db.php';
 
 /* random_bytes NUM
  * Return NUM bytes of random data from /dev/random. */
 function random_bytes($num) {
+    if (!file_exists('/dev/random')) { // probably Windows. Use database.
+        $res = '';
+        while (strlen($res) < $num)
+            $res .= db_getOne('select chr((256*random())::int4)');
+        return $res;
+    }
+    
     global $random_bytes_filehandle;
     if ($num < 0)
         err("NUM must be nonnegative in random_bytes");
@@ -28,6 +38,9 @@ function random_bytes($num) {
 /* urandom_bytes NUM
  * Return NUM bytes of (partly pseudo) random data from /dev/urandom. */
 function urandom_bytes($num) {
+    if (!file_exists('/dev/urandom'))
+        return random_bytes($num);
+
     global $urandom_bytes_filehandle;
     if ($num < 0)
         err("NUM must be nonnegative in urandom_bytes");
