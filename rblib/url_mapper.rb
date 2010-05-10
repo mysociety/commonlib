6a8e88ca693599bae1d394dc79e91c9f11248c2b
Louise Crow <louise.crow@gmail.com>
2010-05-10 14:23:01 +0100

    A couple of functions to allow transformation of relative URLs to their absolute form in either the main or admin interface of a web application.

---
+++ rblib/url_mapper.rb
@@ -0,0 +1,44 @@
+# url_mapper.rb:
+#
+# Some functions for transforming relative URLs within an application
+# to absolute URLs pointing at either the main application or the admin 
+# interface (assuming this may be accessed via a different host).
+#
+# Copyright (c) 2010 UK Citizens Online Democracy. All rights reserved.
+# Email: louise@mysociety.org; WWW: http://www.mysociety.org/
+#
+module MySociety
+
+  module UrlMapper
+  
+    def self.included(base) 
+      base.extend ClassMethods
+    end
+  
+    module ClassMethods
+      
+      def url_mapper
+        include InstanceMethods
+      end
+
+    end
+  
+    module InstanceMethods
+      
+      # Removes any leading /admin directory from the URL, and prefixes it with
+      # the prefix defined in ADMIN_BASE_URL in the config
+      def admin_url(relative_path)
+        admin_url_prefix = MySociety::Config.get("ADMIN_BASE_URL", "/admin/")
+        relative_path = relative_path.gsub(/^\/admin(\/|$)/, '') 
+        return URL.join(admin_url_prefix, relative_path)
+      end
+
+      # Prefixes a relative URL with the domain definted in DOMAIN in the config
+      def main_url(relative_path)
+        url_prefix = "http://" + MySociety::Config.get("DOMAIN", '127.0.0.1:3000')
+        return URI.join(url_prefix, relative_path)
+      end
+      
+    end
+  end
+end
