eb6d0b5e615554d01903e6a8220c0319517489df
Louise Crow <louise.crow@gmail.com>
2010-08-17 14:49:47 +0100

    Adding module for voting area info.

---
+++ rblib/voting_area.rb
@@ -0,0 +1,25 @@
+# voting_area.rb - voting area definitions.
+#
+# Copyright (c) 2010 UK Citizens Online Democracy. All rights reserved.
+# Email: louise@mysociety.org; WWW: http://www.mysociety.org/
+#
+module MySociety
+
+  module VotingArea
+  
+    # va_council_parent_types
+    # Types which are local councils, such as districts, counties,
+    # unitary authorities and boroughs. 
+    def VotingArea.va_council_parent_types
+      ['DIS', 'LBO', 'MTD', 'UTA', 'LGD', 'CTY', 'COI']
+    end
+    
+    # va_council_child_types
+    # Types which are wards or electoral divisions in councils. 
+    def VotingArea.va_council_child_types
+      ['DIW', 'LBW', 'MTW', 'UTE', 'UTW', 'LGE', 'CED', 'COP']
+    end
+    
+  end
+  
+end
