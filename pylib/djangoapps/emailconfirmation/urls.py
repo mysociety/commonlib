46d55fcd17c1086bd221943026423b56aec37f69
Duncan Parkes <duncan@mysociety.org>
2011-06-08 11:02:29 +0100

    Initial Abstract base class stuff.

--- pylib/djangoapps/emailconfirmation/urls.py
+++ pylib/djangoapps/emailconfirmation/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls.defaults import *
 
+# When including these urls, specify model_type to be the class of the model
+# which is superclassing EmailConfirmation, for example
+# urlpatterns = patterns('',
+#     (r'^/', include('emailconfirmation'), {'model_type': Alert}),
+# )
+
 urlpatterns = patterns('emailconfirmation.views',
     url(r'^C/(?P<id>[0-9A-Za-z]+)-(?P<token>.+)$', 'confirm', name='emailconfirmation-confirm'),
     url(r'^D/(?P<id>[0-9A-Za-z]+)-(?P<token>.+)$', 'unsubscribe', name='emailconfirmation-unsubscribe'),
