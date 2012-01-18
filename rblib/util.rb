fec553258f85d181b9441b5f8fb02593956c77ce
Dave Whiteland <dave@mysociety.org>
2012-01-18 12:20:34 +0000

    new by_letter (used by FixMyTransport for pages displaying lists of things organised/tabbed by initial letter)

--- rblib/util.rb
+++ rblib/util.rb
@@ -17,6 +17,30 @@ module MySociety
       rand_num = rand(max = 2**(bits+1)) + 2**bits
       rand_num.to_s(base=36)
     end
+
+    # breaks a list of items into a hash keyed by first letter of their descriptor block
+    # If no block supplied, tries each item's name, or else uses to_s
+    def Util.by_letter(items, force_case=:none)
+      items_by_first = Hash.new { |hash, key| hash[key] = [] }
+      items.each do |item|
+        if block_given?
+          descriptor = yield item
+        elsif item.respond_to?('name')
+          descriptor = item.name
+        else
+          descriptor = item.to_s
+        end
+        first = descriptor[0].chr
+        case force_case
+          when :upcase
+            first.upcase!
+          when :downcase
+            first.downcase!
+        end
+        items_by_first[first] << item
+      end
+      items_by_first
+    end  
     
   end
 
