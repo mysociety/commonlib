b94e1b6af188b96fed88889fa557495f1d650a80
Mark Longair <mhl@pobox.com>
2012-06-22 18:09:14 +0100

    rblib: use jhead -autorot to reorient JPEG images base on EXIF
    It's useful to have this snippet of Ruby which just calls
    jhead -autorot on a file in order to losslessly reorient
    it based on the EXIF orientation data.  (Thanks to Matthew
    Somerville for pointing out that jhead -autorot is simpler
    than the previous version.)

---
+++ rblib/autorotate_image.rb
@@ -0,0 +1,26 @@
+# This is for rotating images to the right orientation based on the
+# EXIF orientation data.  It depends on the binary jhead from the
+# package jhead.
+
+require 'open3'
+
+# If there is no error, this returns nil.  If there was any error, a
+# string with the error is returned (just the captured standard
+# error):
+
+def autorotate_image( filename )
+
+  captured_stderr = nil
+
+  Open3.popen3('jhead', '-autorot', filename) do |input, output, error|
+    captured_stdout = output.read
+    captured_stderr = error.read
+  end
+
+  if captured_stderr.strip.empty?
+    return nil
+  else
+    return captured_stderr
+  end
+
+end
