becf1d484dec2800f4c7a1091694ccaf1f5fc8f6
Ian Chard <ian@mysociety.org>
2013-09-16 10:54:53 +0100

    Don't report E_STRICT errors, which are included in E_ALL from PHP 5.4

--- phplib/error.php
+++ phplib/error.php
@@ -179,10 +179,10 @@ ini_set('ignore_repeated_errors', 'Off');
 ini_set('html_errors', 'Off');
 
 /* Ask for reporting of *all* errors. */
-error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_STRICT);
 
 /* Finally, set our error handler to be the default. Most classes of errors
  * cannot be trapped by a user error-handler, of course. */
-set_error_handler('err_global_handler', E_ALL);
+set_error_handler('err_global_handler', E_ALL & ~E_STRICT);
 
 ?>
