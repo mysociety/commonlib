e4e8225e26934b9b622276b6600d5d90d9996d5b
Matthew Somerville <matthew@mysociety.org>
2016-01-27 19:55:34 +0000

    Update get-pip link to working location.
    Also update documentation to better explain the current situation.

--- bin/get_pip.bash
+++ bin/get_pip.bash
@@ -1,28 +1,19 @@
 #!/bin/bash
 
-# Latest pip requires a newer version of setuptools than we can easily get on
-# Debian. There is some background here:
-#
-# http://stackoverflow.com/questions/20905350/latest-pip-fails-with-requires-setuptools-0-8-for-dist-info
-# https://github.com/pypa/pip/issues/1422
-#
-# We should be able to retire this script once we have upgraded from wheezy
-# to jessie.
-
-# In the meantime, in the script which prepares your environment (often called
-# prepare_environment.bash), you need to run this script just after your
-# virtualenv is activated. If you have commonlib available, you can just run
-# the script, otherwise, use something like
-
-# curl -s https://raw.github.com/mysociety/commonlib/master/bin/get_pip.bash | bash
-
+# This script updates pip and setuptools from whatever you currently have (on
+# Debian wheezy, this is probably 1.1 and 0.6.24). Run this script just after
+# your virtualenv is activated in the script which prepares your environment.
+# If you have commonlib available, you can just run the script; otherwise use
+# something like:
+# curl --silent --location https://raw.github.com/mysociety/commonlib/master/bin/get_pip.bash | bash
 
 # Upgrade pip to a secure version
-curl -s -L https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python
+curl --silent --location https://bootstrap.pypa.io/get-pip.py | python
 
-# The latest pip requires a newer version of setuptools than we have.
-# Since pip version 7.0.0 you can upgrade to the latest version of
-# setuptools by upgrading distribute; for details of this change see
-# https://pip.pypa.io/en/latest/news.html
+# Upgrading distribute would install the latest version of setuptools, but we
+# instead pin to a specific version in case a newer release breaks everything
+# (as 18.6 did).
 pip install setuptools==18.5
+
+# We still want to upgrade distribute to prevent bdist_wheel not found errors
 pip install distribute==0.7.3
