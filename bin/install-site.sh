15230152dde9244c41ac2c41030981ba306a5111
Mark Longair <mhl@pobox.com>
2016-03-21 18:48:42 +0000

    WIP: try out the vagrant-data-import branch for Pombola

--- bin/install-site.sh
+++ bin/install-site.sh
@@ -166,7 +166,7 @@ echo "$HOST" > "$OLD_HOST_FILE"
 REPOSITORY="$DIRECTORY/$SITE"
 
 REPOSITORY_URL=git://github.com/mysociety/$SITE.git
-BRANCH=master
+BRANCH=vagrant-data-import
 
 DISTRIBUTION="$(lsb_release -i -s  | tr A-Z a-z)"
 DISTVERSION="$(lsb_release -c -s)"
