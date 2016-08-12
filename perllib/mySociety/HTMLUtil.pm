276d0758ab001de84823575868e277225a451df1
Matthew Somerville <matthew-github@dracos.co.uk>
2016-08-12 15:40:02 +0100

    Single to double quotes in make_clickable fns.

--- perllib/mySociety/HTMLUtil.pm
+++ perllib/mySociety/HTMLUtil.pm
@@ -39,11 +39,11 @@ sub ms_make_clickable {
     my ($ret) = @_;
     my $contract = 1;
 
-    $ret = ' ' . $ret . ' ';
-    $ret =~ s#(https?)://([^\s<>{}()]+[^\s.,<>{}()])#<a href='$1://$2' rel='nofollow'>$1://$2</a>#ig;
-    $ret =~ s#(\s)www\.([a-z0-9\-]+)((?:\.[a-z0-9\-\~]+)+)((?:/[^ <>{}()\n\r]*[^., <>{}()\n\r])?)#$1<a href='http://www.$2$3$4' rel='nofollow'>www.$2$3$4</a>#ig;
+    $ret = " $ret ";
+    $ret =~ s#(https?)://([^\s<>{}()]+[^\s.,<>{}()])#<a href="$1://$2" rel="nofollow">$1://$2</a>#ig;
+    $ret =~ s#(\s)www\.([a-z0-9\-]+)((?:\.[a-z0-9\-\~]+)+)((?:/[^ <>{}()\n\r]*[^., <>{}()\n\r])?)#$1<a href="http://www.$2$3$4" rel="nofollow">www.$2$3$4</a>#ig;
     if ($contract) {
-        $ret =~ s#(<a href='[^']*'>)([^<]{40})[^<]*?</a>#$1$2...</a>#g;
+        $ret =~ s#(<a href="[^"]*">)([^<]{40})[^<]*?</a>#$1$2...</a>#g;
     }
     $ret =~ s#(\s)([a-z0-9\-_.]+)@([^,< \n\r]*[^.,< \n\r])#$1<a href=\"mailto:$2\@$3\">$2\@$3</a>#gi;
     
