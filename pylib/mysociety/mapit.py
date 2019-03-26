d7fe4bffb132ad54ebc1c1bac547b9c590ddda45
Dave Arter <davea@mysociety.org>
2019-03-26 19:37:29 +0000

    Remove legacy MapIt calls from mapit.py

--- pylib/mysociety/mapit.py
+++ pylib/mysociety/mapit.py
@@ -11,51 +11,6 @@ import urllib2
 import urlparse
 
 import mysociety.config
-import mysociety.rabx
-
-def call_old(*params):
-    base_url = mysociety.config.get("MAPIT_URL")
-    return mysociety.rabx.call_rest_rabx(base_url, params)
-
-BAD_POSTCODE = 2001        #    String is not in the correct format for a postcode. 
-POSTCODE_NOT_FOUND = 2002        #    The postcode was not found in the database. 
-AREA_NOT_FOUND = 2003        #    The area ID refers to a non-existent area. 
-
-def get_voting_areas(postcode):
-    result = call_old('get_voting_areas', postcode)
-    return result
-
-def get_voting_area_info(area):
-    result = call_old('get_voting_area_info', area)
-    return result
-
-def get_voting_areas_info(ary):
-    result = call_old('get_voting_areas_info', ary)
-    return result
-
-def get_voting_area_by_name(name, type = None, min_generation = None):
-    result = call_old('get_voting_area_by_name', name, type, min_generation)
-    return result
-
-def get_voting_areas_by_location(coordinate, method, types = None, generation = None):
-    result = call_old('get_voting_areas_by_location', coordinate, method, types, generation)
-    return result
-
-def get_areas_by_type(type, min_generation = None):
-    result = call_old('get_areas_by_type', type, min_generation)
-    return result
-
-def get_example_postcode(id):
-    result = call_old('get_example_postcode', id)
-    return result
-
-def get_voting_area_children(id):
-    result = call_old('get_voting_area_children', id)
-    return result
-
-def get_location(postcode, partial = None):
-    result = call_old('get_location', postcode, partial)
-    return result
 
 def call(url):
     full_url = urlparse.urljoin(mysociety.config.get('MAPIT_URL'), url)
