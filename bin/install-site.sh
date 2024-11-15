2022cedc2e890806b1899df081467dec4bf2e3d3
Sam Pearson <sam@sgp.me.uk>
2024-11-15 15:43:57 +0000

    Run separately

--- bin/install-site.sh
+++ bin/install-site.sh
@@ -26,6 +26,7 @@ test_ec2() {
     # If we're not running on an EC2 instance, an empty body is returned
     # by this request:
     echo -n "Testing for being on EC2... "
+    curl http://169.254.169.254/latest/meta-data/public-hostname
     EC2_HOSTNAME=`curl --max-time 10 -s http://169.254.169.254/latest/meta-data/public-hostname`
     echo "got ${EC2_HOSTNAME}... ${DONE_MSG}"
 }
